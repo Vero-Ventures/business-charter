35660d69ad0e21739b3a2da30789a3d3e1f3a843
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2024-05-06 21:47:14 -0700

    Add radio button for if contact is a family member or not

--- src/app/contacts/contact-form.tsx
+++ src/app/contacts/contact-form.tsx
@@ -41,6 +41,7 @@ const formSchema = z.object({
       message: "Phone number must be at least 10 characters long",
     })
     .max(15),
+  isFamilyMember: z.boolean().default(false),
 });
 
 export type InsertContact = z.infer<typeof formSchema>;
@@ -54,6 +55,7 @@ export function ContactForm() {
       title: "",
       email: "",
       phone: "",
+      isFamilyMember: false,
     },
   });
 
@@ -123,6 +125,38 @@ export function ContactForm() {
                 <Input placeholder="604-689-8289" type="tel" {...field} />
               </FormControl>
 
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="isFamilyMember"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Is family member?</FormLabel>
+              <FormControl>
+                <div className="flex space-x-4">
+                  <label className="flex items-center space-x-2">
+                    <input
+                      type="radio"
+                      value="Yes"
+                      checked={field.value === true}
+                      onChange={() => field.onChange(true)}
+                    />
+                    <span>Yes</span>
+                  </label>
+                  <label className="flex items-center space-x-2">
+                    <input
+                      type="radio"
+                      value="No"
+                      checked={field.value === false}
+                      onChange={() => field.onChange(false)}
+                    />
+                    <span>No</span>
+                  </label>
+                </div>
+              </FormControl>
               <FormMessage />
             </FormItem>
           )}
