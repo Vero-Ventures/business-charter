1ba680a05ca1dbefbc38b981b7f2a81517825e8b
khagg <kellyahagg@gmail.com>
2024-05-21 16:40:16 -0700

    Lock out non-admin from viewing family-management

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,34 @@
-import { type NextRequest } from 'next/server';
-import { updateSession } from '@/lib/supabase/middleware';
+import { type NextRequest, NextResponse } from 'next/server';
+import { createClient } from '@/lib/supabase/server';
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  // Create the Supabase client within the request context
+  const supabase = createClient();
+
+  // Fetch the session from the cookies
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session && session.user) {
+    const { data: profile } = await supabase
+      .from('profiles')
+      .select('role')
+      .eq('user_id', session.user.id)
+      .single();
+
+    // Check if the user is authorized to access the route
+    if (request.nextUrl.pathname.startsWith('/family-management') && profile?.role !== 'admin') {
+      return NextResponse.redirect(new URL('/unauthorized', request.url));
+    }
+  }
+
+  // If no issues, proceed with the request
+  return NextResponse.next();
 }
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
