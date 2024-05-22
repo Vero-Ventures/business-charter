d76ca8db07121765860d5694af691145aa2d5dfc
Jihoon Yoon <jihoonyoon0@gmail.com>
2024-05-22 15:20:27 -0700

    Create basic authentication tests

---
+++ tests/config.ts
@@ -0,0 +1,4 @@
+// Update when site goes live to test live site
+export const config = {
+  BASE_URL: 'localhost:3000',
+};
--- tests/login.test.ts
+++ tests/login.test.ts
@@ -1,4 +1,4 @@
-import { test, chromium } from '@playwright/test';
+import { test, chromium, expect } from '@playwright/test';
 import { config } from './config';
 
 // For testing existing email
@@ -10,41 +10,45 @@ const testData = {
 test('Login with registered email and correct password', async () => {
   const browser = await chromium.launch();
   const page = await browser.newPage();
-  await page.goto(`${config.BASE_URL}/login`);
-
-  const signInEmail = await page.$('input[name="email"]');
-  const signInPassword = await page.$('input[name="password"]');
-
-  await signInEmail?.fill(testData.email);
-  await signInPassword?.fill(testData.password);
-
-  const signInButton = await page.$('button[type="submit"]');
-  await signInButton?.click();
 
   try {
+    await page.goto(`${config.BASE_URL}/login`);
+
+    const signInEmail = await page.$('input[name="email"]');
+    const signInPassword = await page.$('input[name="password"]');
+
+    await signInEmail?.fill(testData.email);
+    await signInPassword?.fill(testData.password);
+
+    const signInButton = await page.$('button[type="submit"]');
+    await signInButton?.click();
     await page.waitForURL('**/decision-tree');
-    console.log('Login with registered email passed');
+    await expect(page).toHaveURL(/.*\/decision-tree/, { timeout: 5000 });
+    console.log('Login with registered email and correct password passed');
   } catch (error) {
-    console.error('Login with registered email failed');
+    console.error('Login with registered email and correct password failed');
+    throw error;
+  } finally {
+    await browser.close();
   }
 });
 
 test('Login with unregistered email', async () => {
   const browser = await chromium.launch();
   const page = await browser.newPage();
-  await page.goto(`${config.BASE_URL}/login`);
-
-  const signInEmail = await page.$('input[name="email"]');
-  const signInPassword = await page.$('input[name="password"]');
-
-  // TODO: Replace this with a test email and password
-  await signInEmail?.fill('');
-  await signInPassword?.fill('');
-
-  const signInButton = await page.$('button[type="submit"]');
-  await signInButton?.click();
 
   try {
+    await page.goto(`${config.BASE_URL}/login`);
+
+    const signInEmail = await page.$('input[name="email"]');
+    const signInPassword = await page.$('input[name="password"]');
+
+    // TODO: Replace this with a test email and password
+    await signInEmail?.fill('');
+    await signInPassword?.fill('');
+
+    const signInButton = await page.$('button[type="submit"]');
+    await signInButton?.click();
     await page.waitForSelector(
       'text="You entered the wrong email or password."',
       { state: 'visible' }
@@ -52,24 +56,27 @@ test('Login with unregistered email', async () => {
     console.log('Sign-in with unregistered email passed');
   } catch (error) {
     console.error('Sign-in with unregistered email failed');
+    throw error;
+  } finally {
+    await browser.close();
   }
 });
 
 test('Login with registered email and incorrect password', async () => {
   const browser = await chromium.launch();
   const page = await browser.newPage();
-  await page.goto(`${config.BASE_URL}/login`);
-
-  const signInEmail = await page.$('input[name="email"]');
-  const signInPassword = await page.$('input[name="password"]');
-
-  await signInEmail?.fill(testData.email);
-  await signInPassword?.fill(testData.password + '1');
-
-  const signInButton = await page.$('button[type="submit"]');
-  await signInButton?.click();
 
   try {
+    await page.goto(`${config.BASE_URL}/login`);
+
+    const signInEmail = await page.$('input[name="email"]');
+    const signInPassword = await page.$('input[name="password"]');
+
+    await signInEmail?.fill(testData.email);
+    await signInPassword?.fill(testData.password + '1');
+
+    const signInButton = await page.$('button[type="submit"]');
+    await signInButton?.click();
     await page.waitForSelector(
       'text="You entered the wrong email or password."',
       { state: 'visible' }
@@ -77,5 +84,8 @@ test('Login with registered email and incorrect password', async () => {
     console.log('Login with registered email and incorrect password passed');
   } catch (error) {
     console.error('Login with registered email and incorrect password failed');
+    throw error;
+  } finally {
+    await browser.close();
   }
 });
