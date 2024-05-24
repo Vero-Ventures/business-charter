1749d71448cbc43efdc439cba00927e10c7d49c2
khagg <kellyahagg@gmail.com>
2024-05-24 02:10:14 -0700

    Add UI separators for clarity and appearance

--- src/app/family-management/page.tsx
+++ src/app/family-management/page.tsx
@@ -3,6 +3,8 @@ import Loading from '@/components/loading';
 import FamilyMembers from './family-members';
 import { FamilyForm } from './family-form';
 import Families from './families';
+import { Separator } from '../../components/ui/separator';
+
 import AuthenticatedRoute from '../(auth)/authenticated-route';
 
 export default function FamilyPage() {
@@ -11,10 +13,13 @@ export default function FamilyPage() {
       <div className="prose mt-8">
         <main className="min-h-screen px-6 pt-5">
           <h1 className="text-3xl font-bold">Family Management</h1>
+          <Separator className="mx-auto mt-2 w-[100%]" />
+
           <h2 className="text-2xl">Your Families</h2>
           <Suspense fallback={<Loading />}>
             <Families />
           </Suspense>
+          <Separator className="mx-auto mt-2 w-[100%]" />
           <FamilyForm />
         </main>
       </div>
