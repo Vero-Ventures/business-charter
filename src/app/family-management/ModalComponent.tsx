3e1bbdfdc5a2e568da23300cd81de68cef5c5f9a
khagg <kellyahagg@gmail.com>
2024-05-23 19:40:58 -0700

    Correct incorrect update error for adding members

--- src/app/family-management/ModalComponent.tsx
+++ src/app/family-management/ModalComponent.tsx
@@ -29,17 +29,16 @@ const ModalComponent: React.FC<ModalComponentProps> = ({ isOpen, onRequestClose,
   const handleAddFamilyMember = async () => {
     try {
       const supabase = createClient();
-      const { data: userData } = await supabase.auth.getUser();
       const { data: contactsData, error } = await supabase
         .from('profiles')
         .update({ family_id: family.id })
-        .eq('email', userData.user?.email);
+        .eq('email', email); // Use the entered email directly
   
       if (error) {
         console.error('Error updating user:', error);
         alert('Error updating user: ' + error.message);
       } else {
-        console.log('User updated:');
+        console.log('User updated:', contactsData);
         alert('Family member added successfully');
       }
     } catch (err) {
@@ -64,7 +63,7 @@ const ModalComponent: React.FC<ModalComponentProps> = ({ isOpen, onRequestClose,
       <h1 className="text-3xl font-bold"> {name} Family</h1>
       <Suspense fallback={<Loading />}>
         <FamilyMembers />
-      </Suspense>v
+      </Suspense>
       <h2>Add Family Member</h2>
       <input
         type="email"
