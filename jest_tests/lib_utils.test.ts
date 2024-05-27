bf3f44cfbdecc16cbde4ea8a82ef100c1995ebd1
hiem55 <oh.justin55@gmail.com>
2024-05-27 16:46:40 -0700

    lib utils test cases (#27)

---
+++ jest_tests/lib_utils.test.ts
@@ -0,0 +1,23 @@
+import { cn, moneyFormatter } from '../src/lib/utils';
+import { twMerge } from 'tailwind-merge';
+import { clsx } from 'clsx';
+
+describe('cn function', () => {
+  it('should handle empty input', () => {
+    const result = cn();
+    expect(result).toEqual('');
+  });
+  it('should merge class names using twMerge and clsx', () => {
+    const classNames = ['class1', 'class2', 'class3'];
+    const result = cn(...classNames);
+    const expected = twMerge(clsx(...classNames));
+    expect(result).toEqual(expected);
+  });
+});
+
+describe('moneyFormatter function', () => {
+  it('should format money correctly', () => {
+    const result = moneyFormatter.format(1000);
+    expect(result).toEqual('$1,000.00');
+  });
+});
