7a940edd56a152f40f17a63c6f388f4e80e4f546
mimikwkk <113390005+mimikwkk@users.noreply.github.com>
2024-05-26 17:26:09 -0700

    Add authentication route

--- src/app/family-charter/page.tsx
+++ src/app/family-charter/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import { CharterItem } from './types';
+// import AuthenticatedRoute from '../(auth)/authenticated-route';
 
 const FamilyCharter = () => {
     const [charterData, setCharterData] = useState<CharterItem[]>([]);
@@ -10,25 +11,27 @@ const FamilyCharter = () => {
         fetch('/family_charter.json')  // Assuming the JSON file is in the public directory
             .then(response => response.json())
             .then(data => {
-                setCharterData(data.items); // Adjust according to your JSON structure
+                setCharterData(data.items); // Adjust according to JSON structure
             })
             .catch(error => console.error('Failed to load data', error));
     }, []);
 
     return (
-        <div>
-            <h1>Family Charter</h1>
-            {charterData.length > 0 ? (
-                charterData.map((item, index) => (
-                    <div key={index}>
-                        <h2>{item.title}</h2>
-                        <p>{item.description}</p>
-                    </div>
-                ))
-            ) : (
-                <p>No data found</p>
-            )}
-        </div>
+        // <AuthenticatedRoute>
+            <div>
+                <h1>Family Charter</h1>
+                {charterData.length > 0 ? (
+                    charterData.map((item, index) => (
+                        <div key={index}>
+                            <h2>{item.title}</h2>
+                            <p>{item.description}</p>
+                        </div>
+                    ))
+                ) : (
+                    <p>No data found</p>
+                )}
+            </div>
+        // </AuthenticatedRoute>
     );
 };
 
