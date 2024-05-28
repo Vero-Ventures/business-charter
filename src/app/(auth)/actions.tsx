b0372a19f5a7546697c43be9a5e1a55a47c634f5
Amir Eskandari <113070116+am-eskandari@users.noreply.github.com>
2024-05-27 17:49:42 -0700

    Store ID using supabase auth token

--- src/app/(auth)/actions.tsx
+++ src/app/(auth)/actions.tsx
@@ -2,16 +2,30 @@
 
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import jwt from 'jsonwebtoken';
 
 import { createClient } from '@/lib/supabase/server';
+
+interface JWTToken {
+  sub: string;
+}
 
 export async function login(email: string, password: string) {
   const supabase = createClient();
 
-  const { error } = await supabase.auth.signInWithPassword({ email, password });
+  const { error, data } = await supabase.auth.signInWithPassword({ email, password });
 
   if (error) {
     return { message: error.message };
+  }
+
+  const token = data?.session?.access_token;
+  if (token) {
+    const decodedToken = jwt.decode(token) as JWTToken;
+    const userId = decodedToken.sub;
+
+    // Now you can use the userId for any additional logic
+    console.log('User ID:', userId);
   }
 
   revalidatePath('/');
@@ -27,7 +41,7 @@ export async function signup(email: string, password: string, role: string) {
     return { message: error.message };
   }
 
-  const userId = data.user?.id;
+  const userId = data?.user?.id;
   if (userId) {
     const { error: profileError } = await supabase
       .from('profiles')
